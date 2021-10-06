f61b2bc944ed515d7d397869478301708740959a
Besir Gunduz <besirgunduz1993@gmail.com>
2021-10-06 20:58:30 +0300

    commit 002

---
+++ js/002-degisken-tanimlama.js
@@ -0,0 +1,16 @@
+// var - kullanmamaya çalış
+var serverName = "api.besirgunduz.org"
+console.log(serverName)
+
+// let - hataların ne olduğunu net olarak anlayabiliriz
+let fullName;
+fullName = "Beşir Gündüz";
+console.log(fullName)
+
+// birlestirme işlemi
+fullName += " Eklenen"
+console.log(fullName)
+
+// const - ilk değer verilmesi gerekiyor - sonradan değiştirilemez
+const SERVER_PASSWORD = "abcdef123456"
+console.log(SERVER_PASSWORD)
