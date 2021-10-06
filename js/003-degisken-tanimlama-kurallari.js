b3d8c660b9b47dee102e78e530fed1ecdba61691
Besir Gunduz <besirgunduz1993@gmail.com>
2021-10-06 21:50:37 +0300

    commit 00

---
+++ js/003-degisken-tanimlama-kurallari.js
@@ -0,0 +1,16 @@
+// camelCase Kullanimi:
+let kodluyoruzServerInfo = ""
+let firstName = "Beşir"
+let lastName = "Gündüz"
+console.log(firstName, lastName)
+
+// UPPER_CASE Kullanimi:
+const PASSWORD = "1234"
+const SERVER_PASSWORD = "1234"
+
+// x, y, z, k, i, e, gibi anlamsiz degisken isimlerini kullanma
+let x = 1; // yanlış kullanım
+
+// boolean tanimlarken is/has kullanimi:
+let isActive = true
+let hasPassword = false
