1f8730a4b8c7dbe05d02d9bae1ac2e7029c92bbf
Besir Gunduz <besirgunduz1993@gmail.com>
2021-10-14 21:23:24 +0300

    commit 010

---
+++ js/010-dom-icerisinden-oge-secimi.js
@@ -0,0 +1,13 @@
+// ********** DOM Icinden Oge Secimi **********
+
+let title=document.getElementById("title");
+title.innerHTML = "Yeni mesaj";
+
+let link=document.querySelector("#link")
+
+link.innerHTML="Link değişti";
+link.style.color="red";
+
+document.getElementsByTagName("p");
+
+document.getElementsByClassName('kusu');
