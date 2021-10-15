3bdc193c98b053b95826a5a1f0f710e77c3198b6
Besir Gunduz <besirgunduz1993@gmail.com>
2021-10-15 16:08:41 +0300

    commit 013

--- js/013-css-class-eklemek-veya-cikartmak.js
+++ js/013-css-class-eklemek-veya-cikartmak.js
@@ -1,12 +1,53 @@
 // ********** css class eklemek veya cikartmak **********
 
+//JavaScrip’te CSS classlarını manipüle etmenin en iyi yollarından biri classList kullanmaktır. 
+
 let greeting = document.querySelector("#greeting")
+console.log(greeting);
 
-greeting.classList.add("text-primary")
-greeting.classList.add("title")
-greeting.classList.add("new-info", "second-class", "third-class") // birden fazla class eklemek
+greeting.classList.add("text-primary");
+greeting.classList.add("title");
+
+//birden fazla class eklemek
+greeting.classList.add("new-info", "second-class", "third-class")
 
 
-greeting.classList.remove("title", "second-class", "third-class") // birden fazla class silmek
+greeting.classList.remove("title", "second-class", "third-class") 
 
-console.log(greeting.classList)
+console.log(greeting.classList.length)
+
+//örnek
+
+//Add
+const div=document.getElementById("add");
+div.classList.add("fancy","add","addMetod");
+
+const classNames = div.classList;
+document.getElementById("pAdd").innerHTML="Class ekledikten sonra liste: "+ classNames;
+
+//Remove
+div.classList.remove("addMetod");
+document.getElementById("pRemove").innerHTML="Class sildikten sonra liste: "+ classNames;
+
+//Item
+ const firstClassName=div.classList.item(0);
+ document.getElementById("pItem").innerHTML= "İlk class: "+ firstClassName;
+
+//Contains
+const result=div.classList.contains("add");
+document.getElementById("pContainsTrue").innerHTML="'add' class'ını içeriyor mu? "+" --> "+ result;
+const result2=div.classList.contains("addMetod");
+document.getElementById("pContainsFalse").innerHTML="'addMetod' class'ını içeriyor mu? "+" --> "+ result2;
+
+//Replace
+div.classList.replace("add","newClass");
+document.getElementById("pReplace").innerHTML=classNames;
+
+//Toggle
+const pToggle=document.getElementById("pToggle")
+const button=document.getElementById("toggleButton");
+button.addEventListener("click",function(){
+pToggle.classList.toggle("colorp");
+})
+//Length
+document.getElementById("length").innerHTML="Class names length:"+" "+div.classList.length;
