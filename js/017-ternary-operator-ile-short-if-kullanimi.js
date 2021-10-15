31103ca9d09c60828e67886f46d8e9ccdcf492d5
Besir Gunduz <besirgunduz1993@gmail.com>
2021-10-16 00:45:46 +0300

    commit 017

--- js/017-ternary-operator-ile-short-if-kullanimi.js
+++ js/017-ternary-operator-ile-short-if-kullanimi.js
@@ -1,13 +1,22 @@
 // ********** ternary operator ile short if kullanimi **********
 
-// eger kullanici adin varsa yazdir yoksa kullanici bilginiz bulunamadi yaz
+// ternary kullanimi:
+// kosul ? dogruysa : yanlissa
 
 let userName = prompt("Kullanici Bilginiz Yaziniz")
 let info = document.querySelector("#info")
 
-// ternary kullanimi:
-// kosul ? dogruysa : yanlissa
-
-// userName.length > 0 ? userName : "Kullani Bilginiz Bulunamadi :("
-
 info.innerHTML = `${userName ? userName : "Kullani Bilginiz Bulunamadi :("}`
+
+let money=40;
+let canBuy= (money<17) ? "Satın alamazsın" : (money>=40)? "Satın alabilirsin" : "Hata"
+console.log(canBuy);
+
+let boolean;
+
+const isBooleanTrue = boolean ? true : false;
+
+console.log(isBooleanTrue)
+>> false
+
+console.log(boolean)
