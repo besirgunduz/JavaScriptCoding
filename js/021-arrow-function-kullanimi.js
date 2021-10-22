fb51b49660f3a51d3788356d167ce22f881d771d
Besir Gunduz <besirgunduz1993@gmail.com>
2021-10-22 15:03:48 +0300

    commit 021

--- js/021-arrow-function-kullanimi.js
+++ js/021-arrow-function-kullanimi.js
@@ -1,30 +1,71 @@
 // Arrow function Kullanimi
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
 
+//normal fonksiyon
 function hello(firstName) {
-    console.log(`Merhaba ${firstName}`)
+  console.log(`Merhaba ${firstName}`);
 }
+hello("C# and JS");
 
-hello("JavaScript")
+//Arrow kullanım
+const helloFuncV1 = (firstName) => {
+  console.log(`Merhaba ${firstName}`);
+};
+helloFuncV1("Beşir");
 
-const helloFuncV1 = (firstName) => { console.log(`Merhaba ${firstName}`) } 
-helloFuncV1("helloFuncV1")
+const helloFuncV2 = (firstName) => console.log(`Merhaba ${firstName}`);
+helloFuncV2("Ahmet");
 
+const helloFuncV3 = (firstName, lastName) =>
+  console.log(`Merhaba ${firstName} ${lastName}`);
 
-const helloFuncV2 = firstName => console.log(`Merhaba ${firstName}`) // bir parametre, bir donus islemi
-helloFuncV2("helloFuncV2")
-
-
-const helloFuncV3 = (firstName, lastName) => console.log(
-    `Merhaba ${firstName} ${lastName}`
-) // console.log parantezi
-
-helloFuncV3("helloFuncV3", "Last Name info")
+helloFuncV3("Beşir", "Gündüz");
 
 const helloFuncV4 = (firstName, lastName) => {
-    let info = `Merhaba ${firstName} ${lastName}`
-    console.log(info) 
-    return info
-}
+  let info = `Merhaba ${firstName} ${lastName}`;
+  console.log(info);
+  return info;
+};
 
-helloFuncV4("helloFuncV4", "Other Info")
+helloFuncV4("x", "y");
+
+//örnekler
+
+let toplam = (x, y) => x + y;
+
+let sayi = (x) => x * x;
+
+const multiplier = (item, multi) => item * multi;
+
+const myList = (param1, param2) => param1.concat(param2);
+myList([1, 2], [3, 4, 5]); // output : [ 1, 2, 3, 4, 5 ]
+
+let feedTheCat = (cat) => {
+  if (cat === "hungry") {
+    return "Feed the cat";
+  } else {
+    return "Do not feed the cat";
+  }
+};
+
+/*örnek */
+const seriesList = (list) => {
+  list.forEach((series, index) => {
+    console.log(`${index + 1}. ${series}`);
+  });
+};
+
+seriesList(["A", "B", "C"]);
+
+/*örnek */
+const newArray = (nums) => {
+  const newNums = nums.map((e) => {
+    if (e % 2 == 0) {
+      return e * 2;
+    } else if (e % 2 == 1) {
+      return e * 3;
+    }
+  });
+  return newNums;
+};
+
+console.log(newArray([1, 2, 3, 4, 5]));
