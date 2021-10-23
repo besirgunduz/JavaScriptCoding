f20a86fad94e092fc8bac8af62956bf73916a238
Besir Gunduz <besirgunduz1993@gmail.com>
2021-10-24 00:35:20 +0300

    commit 027

--- js/027-form-submit-yonetimi.js
+++ js/027-form-submit-yonetimi.js
@@ -1,9 +1,27 @@
 // FORM SUBMIT:
+//Form içinde bulundurduğu form elementlerinin name'leri sayesinde değerlerini alıp bu değerlerle işlem yapmanıza olanak sağlayan bir yapıdır.
+//Genellikle backend'e istek yapılacağı zaman kullanılır.(Veri göndermek gibi)
 
-let formDOM = document.querySelector("#userForm")
-formDOM.addEventListener('submit', formSubmit)
+let formDOM = document.querySelector("#userForm");
+formDOM.addEventListener("submit", formSubmit);
 
 function formSubmit(event) {
-    event.preventDefault() // default islemi engelledik...
-    console.log("islem gerceklesti")
+  event.preventDefault(); // default islemi engelledik...
+  console.log("islem gerceklesti");
+}
+
+//örnek
+var username;
+var password;
+function handleSubmit(e) {
+  e.preventDefault();
+  console.log("username:" + username + "-" + "password:" + password);
+}
+function handleChange(e) {
+  if (e.target.name === "username") {
+    username = e.target.value;
+  }
+  if (e.target.name === "password") {
+    password = e.target.value;
+  }
 }
