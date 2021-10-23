57b1eb20ccedd6d6f00488723d6397949e830942
Besir Gunduz <besirgunduz1993@gmail.com>
2021-10-23 17:48:45 +0300

    commit 022

--- js/022-dom-etkinlikleriyle-calismak.js
+++ js/022-dom-etkinlikleriyle-calismak.js
@@ -1,11 +1,25 @@
 // DOM Etkinlikleriyle Calismak
-// https://www.w3schools.com/jsref/dom_obj_event.asp
-// https://developer.mozilla.org/en-US/docs/Web/Events
 
-let greeting = document.querySelector("#greeting")
-greeting.addEventListener("click", domClick)
+let greeting = document.querySelector("#greeting");
+greeting.addEventListener("click", domClick);
 
 function domClick() {
-    console.log("etkinlik denetlendi")
-    this.style.color == "red" ? this.style.color = "black" : this.style.color = "red"
+  console.log("etkinlik denetlendi");
+  this.style.color == "red"
+    ? (this.style.color = "black")
+    : (this.style.color = "red");
 }
+
+let input = document.getElementById("input");
+let output = document.getElementById("output");
+
+input.addEventListener("input", function (event) {
+  output.innerText = event.target.value.split("").join("");
+});
+
+let yellowball = document.getElementById("yellow");
+let purpleBall = document.getElementById("purple");
+
+yellowball.onclick = function () {
+  purpleBall.style.backgroundColor = "yellow";
+};
