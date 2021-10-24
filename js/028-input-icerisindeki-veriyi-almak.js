39bdc0af23a2830d3e3fef6f933c0e088246bf43
Besir Gunduz <besirgunduz1993@gmail.com>
2021-10-24 11:16:28 +0300

    commit 028

--- js/028-input-icerisindeki-veriyi-almak.js
+++ js/028-input-icerisindeki-veriyi-almak.js
@@ -1,14 +1,20 @@
 // INPUT icindeki degeri(value) almak
 
-let formDOM = document.querySelector("#userForm")
-formDOM.addEventListener('submit', formSubmit)
-
+let formDOM = document.querySelector("#userForm");
+formDOM.addEventListener("submit", formSubmit);
 
 function formSubmit(event) {
-    event.preventDefault() // default islemi engelledik...
-    console.log("islem gerceklesti")
-    
-    let scoreInputDOM = document.querySelector('#score')
-    console.log(scoreInputDOM.value)
-    localStorage.setItem('score', scoreInputDOM.value)
+  event.preventDefault(); // default islemi engelledik...
+  console.log("islem gerceklesti");
+
+  let scoreInputDOM = document.querySelector("#score");
+  console.log(scoreInputDOM.value);
+  localStorage.setItem("score", scoreInputDOM.value);
 }
+
+//örnek
+const myFunction = () => {
+  var x = document.getElementById("myInput").value;
+  document.getElementById("myP").innerHTML = "Girilen yazı = " + x;
+};
+
