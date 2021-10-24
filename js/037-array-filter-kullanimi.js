59d7f17fbdecd152e165136264fd0ab9e8011992
Besir Gunduz <besirgunduz1993@gmail.com>
2021-10-24 21:18:36 +0300

    commit 037

--- js/037-array-filter-kullanimi.js
+++ js/037-array-filter-kullanimi.js
@@ -1,21 +1,61 @@
 // array filter kullanimi
-// https://developer.mozilla.org/tr/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
 
-// 5 harften fazla olanlar (6) ??
-const PRODUCTS = ["Mic", "Cable", "Speaker", "Desktop PC", "Server", "Mouse", "Keyboard"]
+//Bir Array metodu olan filter(), dizi elemanlarını döngüye alıp,
+//istediğimiz koşula sahip olanlarla yeni bir dizi oluşturmak için kullanılır.
 
-const NEW_PRODUCTS = PRODUCTS.filter(item => item.length > 5)
-console.log(NEW_PRODUCTS)
+// 5 harften fazla olanlar
+const PRODUCTS = [
+  "Mic",
+  "Cable",
+  "Speaker",
+  "Desktop PC",
+  "Server",
+  "Mouse",
+  "Keyboard",
+];
 
+const NEW_PRODUCTS = PRODUCTS.filter((item) => item.length > 5);
+console.log(NEW_PRODUCTS);
 
 // aktif kullanicilar ??
 const USERS = [
-  {fullName: "Ayse Sumer", isActive: false},
-  {fullName: "Ahmet Urgan", isActive: true},
-  {fullName: "Asya Basar", isActive: true},
-  {fullName: "Aksel Durmaz", isActive: false},
-]
+  { fullName: "Beşir Gündüz", isActive: false },
+  { fullName: "Ahmet Urgan", isActive: true },
+  { fullName: "Asya Basar", isActive: true },
+  { fullName: "Aksel Durmaz", isActive: false },
+];
 
-// const ACTIVE_USERS = USERS.filter(user => user.isActive === true)
-const ACTIVE_USERS = USERS.filter(user => user.isActive )
-console.log(ACTIVE_USERS)
+const ACTIVE_USERS = USERS.filter((user) => user.isActive);
+console.log(ACTIVE_USERS);
+
+//M harfiyle başlayan ürünler
+const products = [
+  "Mikrofon",
+  "Kablo",
+  "Telefon",
+  "Bilgisayar",
+  "Mouse",
+  "Klavye",
+  "Ekran",
+];
+const newProducts = products.filter((item) => item.startsWith("M"));
+console.log(newProducts);
+
+//örnek
+const person = [
+  {
+    name: "Beşir",
+    age: 28,
+    languages: ["C#,JavaScript"],
+  },
+  {
+    name: "Ahmet",
+    age: 32,
+    languages: ["C#,Java"],
+  },
+];
+
+const newPerson = person.filter(
+  (person) => person.age > 30 && person.languages.includes("Java")
+);
+console.log(newPerson);
