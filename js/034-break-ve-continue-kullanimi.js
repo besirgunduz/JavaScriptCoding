d556771286785194edb0180e6006a31c8b7d66d5
Besir Gunduz <besirgunduz1993@gmail.com>
2021-10-24 17:53:12 +0300

    commit 034

--- js/034-break-ve-continue-kullanimi.js
+++ js/034-break-ve-continue-kullanimi.js
@@ -1,36 +1,50 @@
-// Break & Continue
-// https://www.w3schools.com/js/js_break.asp
+// Break => işlemi durdur & Continue => devam et
 
 const LOREM_LIST = [
-  'lorem', 'ipsum', 'dolor', 'amet', 'consectetur,', 'adipisicing', 'elit' 
-]
+  "lorem",
+  "ipsum",
+  "dolor",
+  "amet",
+  "consectetur,",
+  "adipisicing",
+  "elit",
+];
 
-let counter = 0
+// let counter = 0;
 
 // for (; counter < 10; counter++) {
 //   console.log(counter)
-//   if (counter === 5) {break}
+//   if (counter === 5) break;
 // }
 
 // for (; counter < 10; counter++) {
-//   if (counter === 5) {continue}
-//   console.log(counter)
+//   if (counter === 5) continue;
+//   console.log(counter);
 // }
 
-const UL_DOM = document.querySelector('#userList')
+// const UL_DOM = document.querySelector("#userList");
 
-let index = 0
+// for (let index = 0; index < LOREM_LIST.length; index++) {
+//   if (LOREM_LIST[index] == "dolor") break;
+//   let LI_DOM = document.createElement("li");
+//   LI_DOM.innerHTML = LOREM_LIST[index];
+//   UL_DOM.append(LI_DOM);
+// }
 
-// for (; index < LOREM_LIST.length; index++) {
-//   if (LOREM_LIST[index] == "dolor") {break}
+// for (let index = 0; index < LOREM_LIST.length; index++) {
+//   if (LOREM_LIST[index] == "dolor") continue;
 //   let LI_DOM = document.createElement('li')
 //   LI_DOM.innerHTML = LOREM_LIST[index]
 //   UL_DOM.append(LI_DOM)
 // }
 
-for (; index < LOREM_LIST.length; index++) {
-  if (LOREM_LIST[index] == "dolor") {continue}
-  let LI_DOM = document.createElement('li')
-  LI_DOM.innerHTML = LOREM_LIST[index]
-  UL_DOM.append(LI_DOM)
+//örnek
+for (var i = 0; i <= 10; i++) {
+  console.log("dış döngü: " + i);
+  for (var j = 0; j <= 5; j++) {
+    if (j == 3) {
+      break;
+    }
+    console.log("iç döngü" + j);
+  }
 }
