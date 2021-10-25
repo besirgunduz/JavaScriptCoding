e42e1a08afb6eb1b61e355c51133e2a213f1f10c
Besir Gunduz <besirgunduz1993@gmail.com>
2021-10-25 15:06:24 +0300

    commit 041

--- js/041-object-icinde-metot-nasil-ekleriz.js
+++ js/041-object-icinde-metot-nasil-ekleriz.js
@@ -3,12 +3,36 @@
 let user1 = {
   firstName: "Bilgin",
   lastName: "Uzman",
-  score: [1, 2, 3, 4], 
+  score: [1, 2, 3, 4],
   isActive: true,
-  shortName: function() {
-    return `${this.firstName[0].toUpperCase()}. ${this.lastName}`
-  }
+  shortName: function () {
+    return `${this.firstName[0].toLowerCase()}. ${this.lastName.toUpperCase()}`;
+  },
 };
 
-console.log(user1)
-console.log(user1.shortName)
+console.log(user1);
+console.log(user1.shortName);
+
+//örnek
+var person = {
+  name: "Beşir",
+  surname: "Gündüz",
+  age: 28,
+  city: "Istanbul",
+  introduce: function () {
+    console.log(`My name is ${this.name} ${this.surname}, I'm ${this.age} yo.`);
+  },
+};
+
+person.myCity = function () {
+  console.log(`I live in ${this.city}`);
+};
+person.myCity();
+
+function Insan(isim, yas) {
+  this.isim = isim;
+  this.yas = yas;
+}
+
+const besir = new Insan("Beşir", 28);
+console.log(besir.yas); //28
