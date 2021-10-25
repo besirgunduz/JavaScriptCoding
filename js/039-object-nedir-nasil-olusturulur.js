3a91afd1011531cd71dc5c51eb77a7a9f980b0a1
Besir Gunduz <besirgunduz1993@gmail.com>
2021-10-25 12:50:33 +0300

    commit 039

--- js/039-object-nedir-nasil-olusturulur.js
+++ js/039-object-nedir-nasil-olusturulur.js
@@ -1,18 +1,31 @@
 // Object(Nesne) Nedir ? Nasıl Oluşturulur?
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object
-let arrayObj = [1, 2, 3]
-let object1 = {obj: 1}
+//Objeler, içerisinde birden fazla değeri depolayan yapılardır.
 
-console.log("arrayObj", typeof(arrayObj))
-console.log("object1", typeof(object1))
+let arrayObj = [1, 2, 3];
+let object1 = { obj: 1 };
 
-let item1 = new Object()
-let item2 = {}
-console.log("item1", typeof(item1))
-console.log("item2", typeof(item2))
+console.log("arrayObj", typeof arrayObj); //object
+console.log("object1", typeof object1); //object
 
 // Object olusturmak
-let item3 = {}
-let item4 = new Object()
+let item1 = {};
+let item2 = new Object();
 
-console.log("object1", object1)
+console.log("item1", typeof item1); //object
+console.log("item2", typeof item2); //object
+
+//örnek
+
+//aynı değerlere sahip 2 primitive veri tipi
+let mesaj = "merhaba";
+let başkaBirMesaj = "merhaba";
+console.log(mesaj == başkaBirMesaj); //true
+
+//aynı değerlere, farklı memory adresslere sahip 2 obje
+let obje = {
+  mesaj: "merhaba",
+};
+let başkaBirObje = {
+  mesaj: "merhaba",
+};
+console.log(obje == başkaBirObje); //false
