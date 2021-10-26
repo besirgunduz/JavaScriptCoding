113263ae4e0929b5acb9cc3ca3de6f11880879e5
Besir Gunduz <besirgunduz1993@gmail.com>
2021-10-26 14:30:06 +0300

    commit 043

--- js/043-hata-yonetimi-icin-try-catch-kullanimi.js
+++ js/043-hata-yonetimi-icin-try-catch-kullanimi.js
@@ -1,20 +1,49 @@
 // hata-yonetimi-icin-try-catch-kullanimi
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/try...catch
-// https://www.w3schools.com/js/js_errors.asp
-
+//Herhangi bir iş yaparken bir hatayla karşılaşılma durumunun ele alınması
 
 let products;
 
 try {
-  products.forEach(item => console.log(item));
+  products.forEach((item) => console.log(item));
 } catch (error) {
-  console.log("error:", error)
+  console.log("error:", error);
   // products = [1, 2, 3]
   // products.forEach(item => console.log(item));
 }
 
+console.log("hata yonetimi duzgun calisiyor");
 
-console.log("hata yonetimi duzgun calisiyor")
+let info = "kodluyoruz";
+console.log(info);
 
-let info = "kodluyoruz"
-console.log(info)
+//örnek
+var messages, number;
+
+function getValue() {
+  messages = document.getElementById("error-message");
+  messages.innerHTML = "";
+  messages.style.color = "red";
+
+  number = document.getElementById("get-value").value;
+
+  try {
+    if (number == "") throw "is Empty";
+    if (isNaN(number)) throw "is not a number";
+    if (number > 10) throw "is too Big";
+    if (number < 5) throw "is too Small";
+
+    debugger;
+
+    if (number == number) {
+      messages.style.color = "green";
+      throw "is " + number;
+    }
+  } catch (err) {
+    messages.innerHTML = "Input " + err;
+  } finally {
+    var test = (document.getElementById("get-value").value = "");
+  }
+  console.log([typeof test, test, number.valueOf()]);
+}
+
+Resources;
