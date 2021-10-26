11689e3bceeb2845144779b4dd5d453a7fa036be
Besir Gunduz <besirgunduz1993@gmail.com>
2021-10-26 20:51:34 +0300

    commit 044

--- js/044-fetch-api-ile-calismak.js
+++ js/044-fetch-api-ile-calismak.js
@@ -1,23 +1,53 @@
 // fetch api ile calismak
-// https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+// Ajax yerine alternatif olarak asenkron veri alıp ve veri göndermemizi sağlar.
 
-// JSON dosyadan veri cekmek:
-fetch("data/settings.json").then(
-  response => response.json()
-).then(responseJson => {
-  console.log(responseJson)
-  console.log(responseJson.userName)
-})
+// let userListDOM = document.querySelector("#userList");
 
-let userListDOM = document.querySelector("#userList")
+// // API uzerinden veri cektik
+// fetch("https://jsonplaceholder.typicode.com/posts")
+//   .then((response) => response.json())
+//   .then((responseJson) => {
+//     responseJson.forEach((item) => {
+//       let liDOM = document.createElement("li");
+//       liDOM.innerHTML = item.id + " " + item.title;
+//       userListDOM.appendChild(liDOM);
+//     });
+//   });
 
-// API uzerinden veri cektik
-fetch("https://jsonplaceholder.typicode.com/posts").then(
-  response => response.json()
-).then(responseJson => {
-  responseJson.forEach(item => {
-    let liDOM = document.createElement('li')
-    liDOM.innerHTML = item.title
-    userListDOM.appendChild(liDOM)
-  })
-})
+// //örnek
+// fetch("https://jsonplaceholder.typicode.com/todos").then((response) =>
+//   response.json().then((todos) => {
+//     todos.forEach((todo) => {
+//       console.log(todo);
+//     });
+//   })
+// );
+
+// //POST İsteği Yapma
+
+// let payload = {
+//   title: "Blog title",
+//   body: "lorem ipsum",
+//   userId: 1,
+// };
+
+// fetch("https://jsonplaceholder.typicode.com/posts", {
+//   method: "POST",
+//   body: JSON.stringify(payload),
+//   headers: { "Content-type": "application/json; charset=UTF-8" },
+// })
+//   .then((response) => response.json())
+//   .then((json = console.log(json)))
+//   .catch((error) => console.log(error));
+
+//örnek
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then((response) => response.json())
+  .then((json) => console.log(json))
+  .catch((err) => console.log(err));
+
+//örnek
+fetch("https://jsonplaceholder.typicode.com/comments")
+  .then((response) => response.json())
+  .then((json) => json.forEach((item) => console.log(item.email)))
+  .catch((err) => console.log(err));
